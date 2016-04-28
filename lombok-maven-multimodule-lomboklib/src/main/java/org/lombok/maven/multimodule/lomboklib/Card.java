826c5d01d754eb8b8690a29ba1a7b2f8f2d45c71
Eero Aaltonen <eero.aaltonen@iki.fi>
2016-04-28 15:40:26 +0300

    Add javadoc descriptions to member fields.

--- lombok-maven-multimodule-lomboklib/src/main/java/org/lombok/maven/multimodule/lomboklib/Card.java
+++ lombok-maven-multimodule-lomboklib/src/main/java/org/lombok/maven/multimodule/lomboklib/Card.java
@@ -16,6 +16,14 @@ public class Card {
 
 	public static enum Suit { CLUBS, DIAMONDS, HEARTS, SPADES }
 	
+	// the field comments are copied to the getters and setters.
+	/**
+	 * {@code Rank} gives a numeric value to a {@code Card}.
+	 */
 	private Rank rank;
+	
+	/**
+	 * {@code Suit} provides a way to group cards, and a secondary valuation.
+	 */
 	private Suit suit;
 }
